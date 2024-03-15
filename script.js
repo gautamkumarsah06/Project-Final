325923e8de4cab75df1dd9a072cd4f5dcb57c21d
gautamkumarsah06 <gautamsah7677@gmail.com>
2024-03-15 10:01:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+// locomotiveAnimation()
+
+var allText = document.querySelectorAll(".page1-text h1")
+
+allText.forEach(function(elem){
+    var text = elem.textContent
+    var splittedText = text.split("")
+    var clutter = ""
+    splittedText.forEach(function(e){
+        clutter += `<span>${e}</span>`
+    })
+    elem.innerHTML = clutter
+})
+
+gsap.from(".page1-text h1 span",{
+    y:300,
+    opacity:0,
+    delay:0.5,
+    duration:0.6,
+    stagger:0.1
+})
